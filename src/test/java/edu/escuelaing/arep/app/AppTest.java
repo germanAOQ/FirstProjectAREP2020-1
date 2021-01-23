a0918af8cd53a9801bbfa3615ae19047bcf4959e
Germán Ospina <german.ospina-q@mail.escuelaing.edu.co>
2021-01-23 12:19:38 -0500

    Añadiendo pruebas unitarias

--- src/test/java/edu/escuelaing/arep/app/AppTest.java
+++ src/test/java/edu/escuelaing/arep/app/AppTest.java
@@ -1,38 +1,108 @@
 package edu.escuelaing.arep.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.*;
+import edu.escuelaing.arep.stat.Stat;
+import edu.escuelaing.arep.stat.exceptions.StatException;
+import edu.escuelaing.arep.util.LinkedList;
+import edu.escuelaing.arep.util.LinkedListIterator;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	
+	@Test
+	public void shouldGetMeanColumnOne() {
+		Double expected = 550.6;
+		LinkedList<Double> lista = new LinkedList<Double>();
+		lista.add(160.0);
+		lista.add(591.0);
+		lista.add(114.0);
+		lista.add(229.0);
+		lista.add(230.0);
+		lista.add(270.0);
+		lista.add(128.0);
+		lista.add(1657.0);
+		lista.add(624.0);
+		lista.add(1503.0);
+		Double actual = 0.0;
+		try {
+			actual = Stat.mean(lista);
+		} catch (StatException e) {
+			e.printStackTrace();
+		}
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void shouldGetMeanColumnTwo() {
+		Double expected = 60.32;
+		LinkedList<Double> lista = new LinkedList<Double>();
+		lista.add(15.0);
+		lista.add(69.9);
+		lista.add(6.5);
+		lista.add(22.4);
+		lista.add(28.4);
+		lista.add(65.9);
+		lista.add(19.4);
+		lista.add(198.7);
+		lista.add(38.8);
+		lista.add(138.2);
+		Double actual = 0.0;
+		try {
+			actual = Stat.mean(lista);
+		} catch (StatException e) {
+			e.printStackTrace();
+		}
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void shouldGetDeviationColumnOne() {
+		Double expected = 572.03;
+		LinkedList<Double> lista = new LinkedList<Double>();
+		lista.add(160.0);
+		lista.add(591.0);
+		lista.add(114.0);
+		lista.add(229.0);
+		lista.add(230.0);
+		lista.add(270.0);
+		lista.add(128.0);
+		lista.add(1657.0);
+		lista.add(624.0);
+		lista.add(1503.0);
+		Double actual = 0.0;
+		try {
+			actual = Stat.stddev(lista);
+		} catch (StatException e) {
+			e.printStackTrace();
+		}
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void shouldGetDeviationColumnTwo() {
+		Double expected = 62.26;
+		LinkedList<Double> lista = new LinkedList<Double>();
+		lista.add(15.0);
+		lista.add(69.9);
+		lista.add(6.5);
+		lista.add(22.4);
+		lista.add(28.4);
+		lista.add(65.9);
+		lista.add(19.4);
+		lista.add(198.7);
+		lista.add(38.8);
+		lista.add(138.2);
+		Double actual = 0.0;
+		try {
+			actual = Stat.stddev(lista);
+		} catch (StatException e) {
+			e.printStackTrace();
+		}
+		assertEquals(expected, actual);
+	}
 }
